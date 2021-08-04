917b9f6e200db2e26b7eeb452829a1c6e5e83b58
oobobeo <gkstmdok@yonsei.ac.kr>
2021-08-04 16:52:36 +0900

    taskdef bugfix

--- src/dynamodb_helper.py
+++ src/dynamodb_helper.py
@@ -6,7 +6,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
-table = os.getenv('DYNAMODB_TABLE')
+table = dynamodb.Table(os.getenv('DYNAMODB_TABLE'))
 
 
 def find_or_create_item(deployment_id, *, pipeline_id=None, task_def=None):
